83cb70da14f48c6cbd5ce5c219a9f34cec460b1b
wk <wangke.snail@gmail.com>
2020-12-14 01:48:26 +0800

    feat: Add title component

---
+++ src/components/ScrollWrapper/MainTittle/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="main-title">
+    <h1>{{ data.field_name }}</h1>
+    <router-link :to="{ path: '/list', query: { field: data.field } }">
+      <span>更多课程 <i class="iconfont icon-arrow-right"></i></span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainTitle",
+  props: {
+    data: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.main-title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 0.44rem;
+  padding: 0 0.1rem;
+  box-sizing: border-box;
+  line-height: 0.44rem;
+
+  h1 {
+    font-size: 0.16rem;
+  }
+
+  span {
+    font-size: 0.12rem;
+    color: #999;
+
+    .iconfont {
+      font-size: 0.1rem;
+    }
+  }
+}
+</style>
--- src/components/ScrollWrapper/index.vue
+++ src/components/ScrollWrapper/index.vue
@@ -2,6 +2,13 @@
   <div ref="wrapper" class="scroll-wrapper">
     <div class="scroll-content">
       <index-swiper :swiper-data="swiperData"></index-swiper>
+      <main-title :data="{ field_name: '推荐课程', field: 'all' }"></main-title>
+      <div v-if="fieldData.length > 0 && courseData.length > 0">
+        <main-title :data="fieldData[0]"></main-title>
+        <main-title :data="fieldData[1]"></main-title>
+        <main-title :data="fieldData[2]"></main-title>
+        <main-title :data="fieldData[3]"></main-title>
+      </div>
     </div>
   </div>
 </template>
@@ -9,12 +16,14 @@
 <script>
 import BetterScroll from "better-scroll";
 import IndexSwiper from "./IndexSwiper";
+import MainTitle from "./MainTittle";
 import { getCourseDatas } from "@/models";
 
 export default {
   name: "IndexScrollWrapper",
   components: {
-    IndexSwiper
+    IndexSwiper,
+    MainTitle
   },
   data () {
     return {
